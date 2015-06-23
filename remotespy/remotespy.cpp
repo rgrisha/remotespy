8d97115e71b1bffa84dd169b3af729d012f96068
rgrisha <rgrisha@yahoo.com>
2015-06-23 11:44:41 +0300

    command line parameter parsing

--- remotespy/remotespy.cpp
+++ remotespy/remotespy.cpp
@@ -18,12 +18,33 @@ using namespace web::http::experimental::listener;
 	// cpprest provides macros for all streams but std::clog in basic_types.h
 
 
+void print_usage(TCHAR* exe) {
+	_tprintf(_T("\nUsage: %s -host <http://hostname:port>\n\n"), exe);
+	_tprintf(_T("To register listener on Windows, use: netsh http add urlacl url=<url> user=<user>\n"));
+	_tprintf(_T("Example: netsh http add urlacl url=http://+:50008/ user=Everyone\n\n"));
+	_tprintf(_T("Do not forget to open port in Windows Firewall if using remote connection\n"));
+}
 
 int _tmain(int argc, _TCHAR* argv[]) {
 
-// Synchronously bind the listener to all nics.
+	TCHAR* hostname = NULL;
+
+	// a simple parameter parser
+	for (int i = 0; i < argc; i++) {
+		if (_tcscmp(argv[i], _T("-host")) == 0 && argc > i + 1) {
+			hostname = argv[i + 1];
+		}
+	}
+
+	if (hostname == NULL) {
+		print_usage(argv[0]);
+		return 1;
+	}
+
+	// Synchronously bind the listener to all nics.
 	uclog << U("Starting listener.") << endl;
-	http_listener listener(U("http://+:50008/json"));
+	//http_listener listener(U("http://localhost:50008/json"));
+	http_listener listener(hostname);
 
 	listener.open().wait();
 
